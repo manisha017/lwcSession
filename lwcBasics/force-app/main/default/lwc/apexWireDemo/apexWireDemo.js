84b0a2d3973f0bb5097b89b87571d756eb0bd30c
Manisha <manisharani2745@gmail.com>
2024-05-13 22:45:22 +0530

    Expose Apex Methods to Lwc

---
+++ lwcBasics/force-app/main/default/lwc/apexWireDemo/apexWireDemo.js
@@ -0,0 +1,7 @@
+import { LightningElement,wire } from 'lwc';
+import getAccountList from '@salesforce/apex/AccountController.getAccountList';
+
+export default class ApexWireDemo extends LightningElement {
+    @wire(getAccountList)
+    accounts;
+}
