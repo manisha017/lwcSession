8cae401c71c59acfd982bf8e9f70cdb27b583eb6
Manisha <manisharani2745@gmail.com>
2024-06-13 08:55:56 +0530

    13thJuneGetRecordUI

--- lwcBasics/force-app/main/default/lwc/getRecordUI/getRecordUI.js
+++ lwcBasics/force-app/main/default/lwc/getRecordUI/getRecordUI.js
@@ -3,22 +3,23 @@ import { getRecordUi } from 'lightning/uiRecordApi';
 
 export default class GetRecordUI extends LightningElement {
 @api recordId;
+formFields =[
+    {fieldName: 'AccountNumber', label : 'Account Number'},
+    {fieldName: 'AnnualRevenue', label : 'Account Revenue'},
+    {fieldName: 'Name', label : 'Name'},
+    {fieldName: 'Phone', label : 'Phone'}
+]
 
     @wire(getRecordUi, { recordIds: '$recordId', layoutTypes: 'Full',modes:'Edit'})
     accHandler({data,error}){
         if(data){
             console.log(data);
+            this.formFields = this.formFields.map(item=>{
+                return {...item,value:data.records[this.recordId].fields[item.fieldName].value}
+            })
         }
         if(error){
 
         }
     }
-  
-
-
-
-
-
-
-
 }
